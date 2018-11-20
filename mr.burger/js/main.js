8c16360f1fd64e6cc0ce6d0fe32656d7bf36ed7d
Zaur Dzhamaldinov <z.dzhamaldinov@gmail.com>
2018-11-21 01:58:47 +0600

    gamburger button

--- mr.burger/js/main.js
+++ mr.burger/js/main.js
@@ -1,5 +1,5 @@
 function mobileMenuControl () {
-  var mobileMenuIcon = document.querySelector('.humb-button');
+  var mobileMenuIcon = document.querySelector('.hamburger-menu');
   var closeMenu = document.querySelector('.close__link');
   var mobileMenu = document.querySelector('.section.mobile-menu');
   var mobileMenuItems = document.querySelectorAll('.nav__item--mobile')
@@ -7,15 +7,13 @@ function mobileMenuControl () {
   mobileMenuIcon.onclick = function(e) {
     e.preventDefault();
     mobileMenu.classList.toggle('mobile-menu--checked');
-  };
-  closeMenu.onclick = function(e) {
-    e.preventDefault();
-    mobileMenu.classList.toggle('mobile-menu--checked');
+    mobileMenuIcon.classList.toggle('hamburger-menu--checked')
   };
   mobileMenuItems.forEach(item => {
     item.onclick = function(e) {
       e.preventDefault();
       mobileMenu.classList.toggle('mobile-menu--checked');
+      mobileMenuIcon.classList.toggle('hamburger-menu--checked')
     };
   });
 }
